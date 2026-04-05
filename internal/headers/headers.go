4052633e1375c8ab253f8ebee1a04248376cc206
dhinesh <dhinesh20022806@gmail.com>
2026-04-05 22:57:52 +0530

    make test case passed

---
+++ internal/headers/headers.go
@@ -0,0 +1,72 @@
+package headers
+
+import (
+	"bytes"
+	"fmt"
+)
+type Headers map[string]string
+
+var rn = []byte("\r\n")
+
+
+func NewHeaders() Headers {
+	return map[string]string{}
+}
+
+func parseHeader(fieldLine []byte) (string, string, error){
+	parts := bytes.SplitN(fieldLine, []byte(":"), 2)
+
+	if len(parts) != 2 {
+		return "", "", fmt.Errorf("malformed field line")
+	}
+
+	name := parts[0]
+	value := bytes.TrimSpace(parts[1])
+
+	fmt.Print(string(value), "value in string")
+
+	if bytes.HasSuffix(name, []byte(" ")){
+		return "", "", fmt.Errorf("malformed field name")
+	}
+
+	return string(name) , string(value), nil
+
+}
+
+func (h Headers) Parse(data []byte) (int,  bool,  error){
+
+	read := 0
+    isDone := false
+	for {
+		idx := bytes.Index(data[read:], rn)
+
+		if idx == -1 {
+			break
+		}
+
+		// /r/n/r/n last /r/n is 0 index
+		if idx == 0 {
+			isDone = true
+			read += len(rn)
+			break
+		}
+
+		name, value, err := parseHeader(data[read:read + idx])
+
+		if err != nil {
+
+			return 0, false, err
+			
+		}
+		fmt.Print(idx, "  idx", read, "  read")
+
+		read += idx + len(rn)
+		h[name] = value
+
+
+	}
+
+	return read, isDone, nil
+} 
+
+
